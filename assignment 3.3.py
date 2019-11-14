0b85e75b80515201c77bbf3524e470932b2bc61a
praveenkumarreddylekkala <lekkala08@gmail.com>
2019-11-14 15:09:27 +0530

    Add files via upload
    3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
    Score Grade
    >= 0.9 A
    >= 0.8 B
    >= 0.7 C
    >= 0.6 D
    < 0.6 F
    If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.

---
+++ assignment 3.3.py
@@ -0,0 +1,28 @@
+3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85. 
+
+
+score = input("Enter Score: ")
+a = float(score)
+if a <= 0.0 :
+    print('The enterd values is lowver of range')
+elif a >= 1.0 :
+    print ('provided value excided the range')
+elif a >= 0.9 :
+    print ('A')
+elif a >= 0.8 :
+    print ('B')
+elif a >= 0.7 :
+    print('C')
+elif a >= 0.6 :
+    print('D')
+elif a < 0.6 :
+    print('F')
+quit()
+     
